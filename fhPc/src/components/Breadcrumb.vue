9a298237fe2431317cbcf1233b83652fffe05cf0
余兴良 <yuxl@bnd.com>
2019-07-30 17:15:42 +0800

    添加分支

---
+++ fhPc/src/components/Breadcrumb.vue
@@ -0,0 +1,28 @@
+<template>
+  <Breadcrumb
+    class="bread"
+    separator=">"
+  >
+    <BreadcrumbItem>面包屑</BreadcrumbItem>
+  </Breadcrumb>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped lang="scss">
+.bread {
+  box-sizing: content-box;
+  color: #797979;
+  font-size: 14px;
+  height: 50px;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 50px;
+  opacity: 0.9;
+  cursor: inherit;
+}
+</style>
---
+++ fhPc/src/main.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import filters from './filters'
+
+
+Vue.config.productionTip = false
+
+// 全局过滤器
+Object.keys(filters).forEach(filterName => {
+    Vue.filter(filterName, filters[filterName])
+})
+
+/* eslint-disable no-new */
+new Vue({
+    router,
+    store,
+    render: h => h(App)
+}).$mount('#app')
